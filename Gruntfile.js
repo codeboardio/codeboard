8d1b21d2b76f32e4d9bfffb1c9ab9d68261a27ff
mict <mict@zhaw.ch>
2019-12-02 08:12:05 +0100

    Update Uglify to `grunt-contrib-uglify-es` to support es6

--- Gruntfile.js
+++ Gruntfile.js
@@ -424,6 +424,8 @@ module.exports = function (grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+
   // Used for delaying livereload until after server has restarted
   grunt.registerTask('wait', function () {
     grunt.log.ok('Waiting for server reload...');
